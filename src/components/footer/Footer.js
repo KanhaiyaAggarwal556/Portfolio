a5d1f864812adf88cb36a78bb7865fe199ccc218
Kanhainya Agrawal <kanhaiaggarwal@gmail.com>
2025-06-29 01:31:09 +0530

    Updation

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,11 +8,11 @@ const Footer = () => {
             <a href="/" className={"footer__logo"} rel="noopener noreferrer">Kanhainya Agrawal</a>
             <ul className={"permalinks"}>
                 <li><a href={"/"} >Home</a></li>
-                <li><a href={"/about"}>About</a></li>
-                <li><a href={"/experiences"}>Experiences</a></li>
-                <li><a href={"/services"}>Services</a></li>
-                <li><a href={"/portfolio"}>Portfolio</a></li>
-                <li><a href={"/contact"}>Contact</a></li>
+                <li><a href={"#about"}>About</a></li>
+                <li><a href={"#experiences"}>Experiences</a></li>
+                <li><a href={"#services"}>Services</a></li>
+                <li><a href={"#portfolio"}>Portfolio</a></li>
+                <li><a href={"#contact"}>Contact</a></li>
                 {/* <li><a href={"#testimonials"}>Testimonials</a></li> */}
             </ul>
 
